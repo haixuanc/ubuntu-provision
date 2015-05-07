0cfb6193834f3499af96843de0045d81f9e0cb87
Haixuan Chen <haixuanc@gmail.com>
2015-05-07 15:22:12 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -40,6 +40,30 @@ fi
 echo "------------------------------------------------------------------------"
 
 # -----------------------------------------------------------------------------
+# Install Docker-compose
+# -----------------------------------------------------------------------------
+
+which curl
+if [ $? -eq 1 ];then
+	sudo apt-get update && sudo apt-get install curl
+fi
+
+curl -L https://github.com/docker/compose/releases/download/1.1.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# [OPTIONAL] Install `docker-compose` command completion
+curl -L https://raw.githubusercontent.com/docker/compose/1.10/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose
+
+docker-compose --version && STATUS=$?
+echo "------------------------------------------------------------------------"
+if [ $STATUS -eq 0 ]; then
+	echo -e "Docker-compose installed successfully"
+else
+	echo -e "Failed to install Docker-compose"
+fi
+echo "------------------------------------------------------------------------"
+
+# -----------------------------------------------------------------------------
 # Install NodeJS
 # -----------------------------------------------------------------------------
 curl -sL https://deb.nodesource.com/setup | sudo bash -
